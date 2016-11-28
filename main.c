de4f8851e7bfa5509cd60a453006c5d24a0260b1
Brandon <bmwisema@ncsu.edu>
2016-11-28 14:20:46 -0500

    Includes skeleton for later code to be added. i2c config still needs to be verified.

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include "uart.h"
 #include "driverlib.h"
 
-#define COMPASS_ADDRESS  (0x3C)
+#define COMPASS_ADDRESS  (0x3C)		//7-bit slave address
 
 const eUSCI_I2C_MasterConfig i2cConfig =
 {
@@ -14,6 +14,14 @@ const eUSCI_I2C_MasterConfig i2cConfig =
         EUSCI_B_I2C_NO_AUTO_STOP                // No Autostop
 };
 
+struct compass_data {
+	int x;
+	int y;
+	int z;
+} compass_data;
+
+double direction =0;
+
 int main(void) {
 	
   Init_Clocks();
@@ -22,12 +30,11 @@ int main(void) {
 	/* Disabling the Watchdog */
     MAP_WDT_A_holdTimer();
 
-    /* Select Port 1 for I2C - Set Pin 6, 7 to input Primary Module Function,
+    /* Select Port 6 for I2C - Set Pin 4, 5 to input Primary Module Function,
      *   (UCB0SIMO/UCB0SDA, UCB0SOMI/UCB0SCL).
      */
-	
-    MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P6,
-            GPIO_PIN4 + GPIO_PIN5, GPIO_PRIMARY_MODULE_FUNCTION);
+		
+    MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P6, GPIO_PIN4 + GPIO_PIN5, GPIO_PRIMARY_MODULE_FUNCTION);
 
     /* Initializing I2C Master to SMCLK at 400kbs with no autostop */
     MAP_I2C_initMaster(EUSCI_B0_BASE, &i2cConfig);
@@ -42,14 +49,19 @@ int main(void) {
     MAP_I2C_enableModule(EUSCI_B0_BASE);
     
     /* Enable and clear the interrupt flag */
-    MAP_I2C_clearInterruptFlag(EUSCI_B0_BASE,
-            EUSCI_B_I2C_TRANSMIT_INTERRUPT0 + EUSCI_B_I2C_NAK_INTERRUPT);
+    MAP_I2C_clearInterruptFlag(EUSCI_B0_BASE, EUSCI_B_I2C_TRANSMIT_INTERRUPT0 + EUSCI_B_I2C_NAK_INTERRUPT);
     //Enable master Receive interrupt
-    MAP_I2C_enableInterrupt(EUSCI_B0_BASE,
-            EUSCI_B_I2C_TRANSMIT_INTERRUPT0 + EUSCI_B_I2C_NAK_INTERRUPT);
+    MAP_I2C_enableInterrupt(EUSCI_B0_BASE, EUSCI_B_I2C_TRANSMIT_INTERRUPT0 + EUSCI_B_I2C_NAK_INTERRUPT);
     MAP_Interrupt_enableInterrupt(INT_EUSCIB0);
   
   while(1) {
-    
+    // 1. send 0x3C 0x00 0x71 - 15Hz default, positive self test measurement
+		// 2. send 0x3C 0x01 0xA0 - gain = 5
+		// 3. send 0x3C 0x02 0x00 - continuous-measurement mode
+		// 4. wait 6ms
+		// 5. Send 0x3D 0x06, convert hex values to x,y,&z
+		// 6. check limts according to gain (see datasheet)
+		// 7. find which LED corresponds to heading
+		// 8. turn off old LED, turn on new LED
   }
 }
